64a871721e0aa60f999e80c51b533d2bc66c4a3a
Anibe <work@anibeagamah.com>
2021-02-22 23:24:27 +0000

    Additional styling added to layout

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,15 @@ import styles from '../styles/Home.module.css'
 
 export default function ProductGrid() {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} text-center`}>
       <Head>
         <title>Dishwashers</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1>Dishwashers</h1>
-        <section>
-
+        <h1 className="leading-10 text-3xl my-4">Dishwashers</h1>
+        <section className={styles.section}>
+            Here the grid will go.
         </section>
       </main>
     </div>
