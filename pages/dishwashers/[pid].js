1bcbb3a1577abc68bf70b9f2e6088cdd9d04b15e
Anibe Agamah <work@anibeagamah.com>
2021-02-25 22:24:15 +0000

    More product detail fields added

--- pages/dishwashers/[pid].js
+++ pages/dishwashers/[pid].js
@@ -22,8 +22,16 @@ export default function ProductDetail() {
       .catch((error) => {
         console.log(error);
       });
-    const { title, price, media } = productData;
-    setProductDetails({ title, price: price.now, images: [media.images.urls[0]], imagesAltText: media.images.altText });
+    const { title, price, media, details, additionalServices, displaySpecialOffer, code } = productData;
+    setProductDetails({
+      title, price: price.now,
+      images: [media.images.urls[0]],
+      imagesAltText: media.images.altText,
+      productInformation: details.productInformation,
+      includedServices: additionalServices.includedServices[0],
+      displaySpecialOffer,
+      code
+    });
   }, []);
 
   return (
@@ -33,30 +41,33 @@ export default function ProductDetail() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className="flex">
+        <div className="flex justify-center items-center leading-10">
           <Link href="/">
-            &laquo; Back
+            <span className="text-4xl px-5 u-grey-text cursor-pointer">
+              &laquo;
+            </span>
           </Link>
-          <h1 className="leading-10 text-3xl my-4">{productDetails.title}</h1>
+          <h1 className="text-3xl my-4">{productDetails.title}</h1>
         </div>
-        <article>
-          <ul className={`container mx-auto flex ${styles.imageCarousel}`}>
+        <article className="text-left u-grey-text">
+          <ul className={`container mx-auto flex text-center ${styles.imageCarousel}`}>
             {productDetails.images.map(image => {
               return (
-                <li key={image} className={`container ${styles.imageCarouselImage}`}>
-                  <img src={image} alt={productDetails.imagesAltText} />
+                <li key={image} className={`container`}>
+                  <img src={image} alt={productDetails.imagesAltText} className={`${styles.imageCarouselImage}`} />
                 </li>
               )
             })}
           </ul>
-          <section>
-            <h2>£{productDetails.price}</h2>
-            <h5 className="u-red-text">Special offer</h5>
-            <h5 className="u-green-text">Special offer</h5>
+          <section className="container mx-auto mb-9 px-5">
+            <h2 className="text-3xl">£{productDetails.price}</h2>
+            <h5 className="u-red-text text-2xl special-offer">{productDetails.displaySpecialOffer}</h5>
+            <h5 className="u-green-text included-services text-2xl">{productDetails.includedServices}</h5>
           </section>
-          <section className="product-information">
-            <h3>Product Information</h3>
-            <p>bla bla</p>
+          <section className="container mx-auto mb-9 px-5">
+            <h3 className="text-2xl mb-3">Product Information</h3>
+            <div className="product-information" dangerouslySetInnerHTML={{__html: productDetails.productInformation }} />
+            <div className="product-code">Product code: {productDetails.code}</div>
           </section>
         </article>
       </main>
